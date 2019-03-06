605dc6224920363c66e914beaa4ff21fcf4d56bd
Kent Kawahara <kkawahara1028@gmail.com>
2019-03-06 15:15:10 -0800

    Fix functions-test
    issue https://github.com/thewells1024/SeniorProject/issues/2

--- test-utils.rkt
+++ test-utils.rkt
@@ -13,7 +13,10 @@
         (export language^)
         (: eval-lang (-> Program Context Computation))
         (define (eval-lang exp context)
-          (error "Bad Program"))
+          (match exp
+            ; This won't fail, the type-checker just deosn't know that Val? enforces a Val
+            [(? Val? val) (cast val Val)]
+            [_ (error "Bad Program")]))
         (: admin-lang (-> Computation Resources (Pairof Val Resources)))
         (define (admin-lang comp resources)
           (error "Bad Admin"))
